df6b31c4b0a30c1db7a578a02d2ae63120365739
nubychan <45997620+nubychan@users.noreply.github.com>
2018-12-20 02:01:57 +0700

    index.php
    Masih Dalam Perbaikan

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<head>
+<title> Test </title>
+<style type="text/css">
+h1 {
+font family:shadow into light;
+text align:center;
+
+}
+</head>
+<body>
+<h1>Halloo Masih Dalam Perbaikan </h1>
+</body>
+</html>
